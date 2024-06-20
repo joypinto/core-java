7ca69dc0c841c812e468f98378c54d399384a0f7
joypinto <joypinto.xworkz@gmail.com>
2024-06-20 14:56:47 +0530

    some name

---
+++ shop.java
@@ -0,0 +1,9 @@
+class shop
+{
+  static String chocolates[]={"perks","diry milk","kitkat","munch","temtation"};
+ static String dress[]={"Alen Soley", "American polo","Levis","Decothlon"};
+ static String watch[]={"Titan","HMT","Sonata","Firebolt","iWatch"};
+ public static void main(String[]shop){
+	 System.out.println ("The Available Aytems are."+chocolates[2]+" "+dress[3]+" "+watch[0]" );
+ }
+}
